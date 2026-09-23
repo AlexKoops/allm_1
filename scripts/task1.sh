c8be2cb1c7ce07e92a1b4dfa17fbe1b87b92798f
AlexKoops <125556152+AlexKoops@users.noreply.github.com>
2026-09-23 13:45:54 +0200

    change order of tokenizers to keep bigger vocab size tokenizer

--- scripts/task1.sh
+++ scripts/task1.sh
@@ -8,11 +8,11 @@ uv run -m nanochat.dataset -n 2
 # Run 1:
 
 # create first tokenizer with vocab-size of 32768
-uv run -m scripts.tok_train --max-chars=500000000 --vocab-size=32768
+uv run -m scripts.tok_train --max-chars=500000000 --vocab-size=8192
 uv run -m scripts.tok_eval
 
 # Run 2:
 
 # create second tokenizer with vocab-size of 8192
-uv run -m scripts.tok_train --max-chars=500000000 --vocab-size=8192
+uv run -m scripts.tok_train --max-chars=500000000 --vocab-size=32768
 uv run -m scripts.tok_eval
