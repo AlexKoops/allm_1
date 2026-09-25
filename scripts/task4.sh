d650ea1266cabfb27226e64710b585b24e0a6b60
AlexKoops <125556152+AlexKoops@users.noreply.github.com>
2026-09-25 09:39:22 +0200

    hope this works

--- scripts/task4.sh
+++ scripts/task4.sh
@@ -2,7 +2,7 @@ export NANOCHAT_BASE_DIR="/local/$(whoami)/allm_data"
 mkdir -p "$NANOCHAT_BASE_DIR"
 echo "NANOCHAT_BASE_DIR=$NANOCHAT_BASE_DIR"
 
-$TEMPERATURE=$1
+TEMPERATURE=$1
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/../nanochat"
