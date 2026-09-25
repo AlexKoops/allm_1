2ec7c63c23d8eb68ab03c6c59d501efb93d91bb6
AlexKoops <125556152+AlexKoops@users.noreply.github.com>
2026-09-25 09:37:20 +0200

    fix variable in task4.sh

--- scripts/task4.sh
+++ scripts/task4.sh
@@ -2,9 +2,9 @@ export NANOCHAT_BASE_DIR="/local/$(whoami)/allm_data"
 mkdir -p "$NANOCHAT_BASE_DIR"
 echo "NANOCHAT_BASE_DIR=$NANOCHAT_BASE_DIR"
 
-TEMPERATURE=$1
+$TEMPERATURE=$1
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/../nanochat"
 
-uv run python -m scripts.chat_cli --temperature=TEMPERATURE
+uv run python -m scripts.chat_cli --temperature=$TEMPERATURE
